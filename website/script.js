36b3f726fa43acbf20d058c21c6c353caef056b0
huhuhuhus <851123707@qq.com>
2025-02-21 11:49:36 +0800

    Initial commit

--- website/script.js
+++ website/script.js
@@ -1,2 +1,9 @@
 // script.js
-console.log('Website loaded successfully!');
+
+// 在控制台输出一些信息
+console.log('EmberValidator Homepage Loaded');
+
+// 示例：模拟显示在线验证者
+const projectsSection = document.getElementById('running-projects');
+projectsSection.innerHTML += `<p>Validator Node 1: Online</p>`;
+projectsSection.innerHTML += `<p>Validator Node 2: Online</p>`;
